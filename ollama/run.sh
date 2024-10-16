f7cc6a2e3fd55aa7fe77fddcc069aff013163f1a
pratikjhaveri <pratik@boardable.com>
2024-10-15 20:00:32 -0600

    remove user defined host and add default model

--- ollama/run.sh
+++ ollama/run.sh
@@ -5,7 +5,6 @@ bashio::log.info "Starting Ollama service on port 11434..."
 
 # Declare variables
 OLLAMA_MODELS=""
-OLLAMA_HOST=""
 OLLAMA_MAX_LOADED_MODELS=""
 OLLAMA_INTEL_GPU=""
 OLLAMA_GPU_OVERHEAD=""
@@ -16,7 +15,6 @@ OLLAMA_LOAD_TIMEOUT=""
 
 # Assign values from Home Assistant add-on config
 OLLAMA_MODELS=$(bashio::config 'model_path')
-OLLAMA_HOST=$(bashio::config 'host')
 OLLAMA_MAX_LOADED_MODELS=$(bashio::config 'max_loaded_models')
 OLLAMA_INTEL_GPU=$(bashio::config 'intel_gpu')
 OLLAMA_GPU_OVERHEAD=$(bashio::config 'gpu_overhead')
@@ -25,21 +23,12 @@ OLLAMA_MAX_QUEUE=$(bashio::config 'max_queue')
 OLLAMA_NUM_PARALLEL=$(bashio::config 'num_parallel')
 OLLAMA_LOAD_TIMEOUT=$(bashio::config 'load_timeout')
 
-# Download models if specified in the configuration
-if bashio::config.has_value 'download_models'; then
-  for model in $(bashio::config 'download_models'); do
-    bashio::log.info "Downloading model: ${model}..."
-    # Replace with actual command to download models
-    ollama download ${model}
-  done
-fi
-
 # Load models if specified in the configuration
 if bashio::config.has_value 'load_models'; then
   for model in $(bashio::config 'load_models'); do
     bashio::log.info "Loading model: ${model}..."
     # Replace with actual command to load models
-    ollama load ${model}
+    ollama run ${model}
   done
 fi
 
